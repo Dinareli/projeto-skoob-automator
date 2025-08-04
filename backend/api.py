05586d6a614da67a28ade3429418fc3af1491ccb
Natalia Dinareli de Lima <natalia@sounuv.com>
2025-08-04 17:19:25 -0300

    fix/ap-Cors

--- backend/api.py
+++ backend/api.py
@@ -12,8 +12,9 @@ import json
 import urllib.parse
 
 # --- INICIALIZAÇÃO DA API ---
-app = Flask(__name__)
-CORS(app) 
+from flask_cors import CORS
+
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
 def get_latest_progress_from_readwise(book_title, readwise_token):
     print(f"-> Buscando progresso para '{book_title}' no Readwise...")
